c95ffdaf4fb4df49d1ff4ca695a63932ea275e99
MatthieuSCORDIA <matth.scordia@efrei.net>
2024-05-23 12:00:08 +0200

    correction range bouton + ajout images prototype

--- fonction.py
+++ fonction.py
@@ -64,7 +64,8 @@ class VolumeSlider:
             self.dragging = False
         elif event.type == pygame.MOUSEMOTION:
             if self.dragging:
-                self.knob_rect.x = min(max(self.rect.x, event.pos[0]), self.rect.x + self.rect.width - self.knob_rect.width)
+                self.knob_rect.x = min(max(self.rect.x, event.pos[0]),
+                                       self.rect.x + self.rect.width - self.knob_rect.width)
                 self.volume = ((self.knob_rect.x - self.rect.x) / self.rect.width) * 100
 
     def draw(self, surface):
@@ -79,19 +80,18 @@ musique = Game()
 
 
 def draw_text(text, font, color, surface, x, y, rect):
-    pygame.draw.rect(surface, GRAY, (x-5-90, y, 55, 25))
-    pygame.draw.rect(surface, GRAY, (x+55, y, 85, 25))
-    pygame.draw.rect(surface, GRAY, (x+145+90, y, 100, 25))
+    pygame.draw.rect(surface, GRAY, (x - 5 - 90, y, 55, 25))
+    pygame.draw.rect(surface, GRAY, (x + 55, y, 85, 25))
+    pygame.draw.rect(surface, GRAY, (x + 145 + 90, y, 100, 25))
     text_obj = font.render(text, True, color)
     text_rect = text_obj.get_rect()
-    text_rect.topleft = (x-90, y)
+    text_rect.topleft = (x - 90, y)
     surface.blit(text_obj, text_rect)
 
 
 class afficher_image:
 
     def __init__(self, num_image):
-
         background_image = pygame.image.load(tab_image[num_image]).convert()
         background_image = pygame.transform.scale(background_image, (window_width, window_height))
         window.blit(background_image, (0, 0))
@@ -119,28 +119,29 @@ class bouton:
     def accueil(self):
         bouton_exit_image = pygame.image.load(bouton_exit).convert()
         bouton_exit_image = pygame.transform.scale(bouton_exit_image, (taille_bouton_x, taille_bouton_y))
-        window.blit(bouton_exit_image, (pos_bouton_x+150, pos_bouton_y))
+        window.blit(bouton_exit_image, (pos_bouton_x + 150, pos_bouton_y))
         bouton_option_image = pygame.image.load(bouton_exit).convert()
         bouton_option_image = pygame.transform.scale(bouton_option_image, (taille_bouton_x, taille_bouton_y))
         window.blit(bouton_option_image, (pos_bouton_x, pos_bouton_y))
         bouton_start_image = pygame.image.load(bouton_exit).convert()
         bouton_start_image = pygame.transform.scale(bouton_start_image, (taille_bouton_x, taille_bouton_y))
-        window.blit(bouton_start_image, (pos_bouton_x-150, pos_bouton_y))
+        window.blit(bouton_start_image, (pos_bouton_x - 150, pos_bouton_y))
 
     def draw(self, surface, rect):
         for index, item in enumerate(self.items):
-            pygame.draw.rect(surface, (255, 0, 0, 128), (255, (200 + index*50)-10, (window_width + index*50)//2, 200 + index*50))
-            draw_text(item, font, BLACK, surface, ((window_width//2)-120), 200 + index*50, rect)
+            pygame.draw.rect(surface, (255, 0, 0, 128),
+                             (255, (200 + index * 50) - 10, (window_width + index * 50) // 2, 200 + index * 50))
+            draw_text(item, font, BLACK, surface, ((window_width // 2) - 120), 200 + index * 50, rect)
 
     def bouton_clicker(self, menu_accueil, x, y, num_image, menu1, rect):
         print(num_image, " , ", x, " , ", y, " , ", menu1)
-        if (num_image == 0 and pos_bouton_x <= x <= 575 and pos_bouton_y <= y <= pos_bouton_y+50
+        if (num_image == 0 and pos_bouton_x <= x <= 575 and pos_bouton_y <= y <= pos_bouton_y + 50
                 and menu1 == 0):
             menu_accueil.draw(window, rect)
             menu1 += 1
             print("test2")
             return menu1
-        elif num_image == 0 and menu1 == 1 and 445 <= x <= 530 and 200 <= y <= 200+25:
+        elif num_image == 0 and menu1 == 1 and 445 <= x <= 530 and 200 <= y <= 200 + 25:
             print("test1")
             afficher_image.__init__(self, 0)
             bouton.accueil(self)
@@ -161,12 +162,13 @@ class bouton:
                 return 0
 
 
-def choice_selection(num_image,mouse_pos):
-    if num_image in [3] and mouse_pos[0]<=512:
+def choice_selection(num_image, mouse_pos):
+    if num_image in [3] and 15 <= mouse_pos[0] <= 340 and 440 <= mouse_pos[1] <= 590:
         return 1
-    elif num_image in [3] and mouse_pos[0]>512:
-        return 2
+    elif num_image in [3] and 615 <= mouse_pos[0] <= 1000 and 425 <= mouse_pos[1] <= 580:
+      return 2
     return 0
+
 
 def image_incrementation(num_image):
     if num_image in [4]:
